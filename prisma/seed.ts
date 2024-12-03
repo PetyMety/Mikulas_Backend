ec244176f9e4df168cae7b703a180e16e54e0264
Peti <nemt451@gmail.com>
2024-12-03 20:13:45 +0100

    schema/seed.ts kész

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,20 +4,33 @@ import { create } from 'domain';
 const prisma = new PrismaClient()
 
 async function main() {
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 20; i++) {
     await prisma.game.create({
       data: {
         name      : faker.string.alphanumeric(),
-        material  : faker.string.alpha(),
+        material  : faker.helpers.arrayElement(['wood', 'metal', 'plastic', 'other']),
         weight    : faker.number.float({min : 0, max : 15}),
-        child: {
-          create {
-            name : faker.person.fullName(),
-            address : faker.location.streetAddress() + ', ' + faker.location.city() + ', ' + faker.location.country(),
-            isGood : faker.datatype.boolean()
-          }
-        }
+      },
+    });
+  };
+
+  for (let i = 0; i < 10; i++) {
+    await prisma.child.create({
+      data: {
+        name: faker.person.fullName(),
+        address: faker.location.secondaryAddress() + ", " + faker.location.country(),
+        isGood: faker.datatype.boolean(),
+        gameId: faker.number.int({min: 1, max:20}),
       }
-    })
-  }
+    });
+  };
 }
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
